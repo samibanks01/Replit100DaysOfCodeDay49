70b24112721bc2333decb38fd814464aae229aaa
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-07 19:21:24 -0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os, time
+
+while True:
+  print("🌟HIGH SCORE TABLE🌟")
+  print()
+  name = input("INITIALS > ").upper()
+  score = input("SCORE > ")
+  print()
+
+  f = open("high.score", "a+")
+  f.write(f"{name} {score}\n")
+  f.close()
+
+  print("ADDED")
+  time.sleep(1)
+  os.system("clear")
